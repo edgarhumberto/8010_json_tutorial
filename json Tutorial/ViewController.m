da452003360f08933eeff9b2db618aa29003943d
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-03-17 19:48:42 -0600

    Json added

--- json Tutorial/ViewController.m
+++ json Tutorial/ViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "SBJson.h"
+
+NSString        *dataPost;
+NSDictionary    *jsonResponse;
+NSMutableArray  *aStudentName;
 
 @interface ViewController ()
 
@@ -17,11 +22,75 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self postService];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/*******************************************************************************
+ Web Service
+ *******************************************************************************/
+//-------------------------------------------------------------------------------
+- (void) postService
+{
+    NSLog(@"postService");
+    NSOperationQueue *queue             = [NSOperationQueue new];
+    NSInvocationOperation *operation    = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(loadService) object:nil];
+    [queue addOperation:operation];
+}
+//-------------------------------------------------------------------------------
+- (void) loadService
+{
+    @try
+    {
+        NSString *post = [[NSString alloc] initWithFormat:@"id=%@", dataPost];
+        NSLog(@"postService: %@",post);
+        NSURL *url = [NSURL URLWithString:@"http://ec2-52-11-227-165.us-west-2.compute.amazonaws.com/"];
+        NSLog(@"URL postService = %@", url);
+        NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+        NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+        [request setURL:url];
+        [request setHTTPMethod:@"POST"];
+        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+        [request setHTTPBody:postData];
+        [NSURLRequest requestWithURL:url];
+        NSError *error = [[NSError alloc] init];
+        NSHTTPURLResponse *response = nil;
+        NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+//-------------------------------------------------------------------------------
+        if ([response statusCode] >=200 && [response statusCode] <300)
+        {
+            jsonResponse = [NSJSONSerialization JSONObjectWithData:urlData options:kNilOptions error:&error];
+        }
+        else
+        {
+            if (error)
+            {
+                NSLog(@"Error");
+                
+            }
+            else
+            {
+                NSLog(@"Conect Fail");
+            }
+        }
+//-------------------------------------------------------------------------------
+    }
+    @catch (NSException * e)
+    {
+        NSLog(@"Exception");
+    }
+//-------------------------------------------------------------------------------
+    NSLog(@"jsonResponse %@", jsonResponse);
+    aStudentName        = [jsonResponse valueForKey:@"studentName"];
+    NSLog(@"aStudentName %@", aStudentName);
+    //[self.tblMain reloadData];
 }
 
 @end
